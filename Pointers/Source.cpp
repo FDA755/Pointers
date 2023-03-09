da35e7afede28e0a5f0b8c3f3c83fc3e19ac5299
svaadmin <svaadmin@DESKTOP-0PCLR79>
2023-03-09 20:20:37 +0300

    Pointers

--- Pointers/Source.cpp
+++ Pointers/Source.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 //#define POINTER_START
-
+//#define POINTERS_2
+#define POINTERS_3
+#ifdef POINTERS_2
 void Exchange(int* a, int* b);
 
 
@@ -13,7 +15,7 @@ void main()
 #ifdef POINTER_START
 	int a = 2;
 	int* pa = &a;
-	cout << a <<endl;
+	cout << a << endl;
 	cout << &a << endl;
 	cout << pa << endl;
 	cout << *pa << endl;
@@ -23,6 +25,7 @@ void main()
 	pb = &b;
 	cout << *pb << endl;
 #endif
+
 	int  a = 2, b = 3;
 	cout << a << "\t" << b << endl;
 	Exchange(&a, &b);
@@ -36,3 +39,29 @@ void Exchange(int* a, int* b)
 	*a = *b;
 	*b = buffer;
 }
+#endif
+#ifdef POINTERS_3
+void Exchange(int& a, int& b);
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int  a = 2, b = 3;
+	cout << a << "\t" << b << endl;
+	Exchange(a, b);
+	cout << a << "\t" << b << endl;
+
+}
+
+void Exchange(int& a, int& b)
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+
+}
+
+#endif
+
